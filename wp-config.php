3fe1ca09d17072f07489de5b435b38e4615b98eb
rickfindlater <rickfindlater@gmail.com>
2015-07-31 17:50:33 +1200

    wp config file

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'svh');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'wp');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'groute17');
+define('DB_PASSWORD', 'sd38dsT3dF');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
