96db79be21be6f49898266dfa212fb413c38617f
rickfindlater <rickfindlater@gmail.com>
2015-09-01 21:39:24 +1200

    homepage template

--- wp-content/themes/svh/templates/page-homepage.php
+++ wp-content/themes/svh/templates/page-homepage.php
@@ -19,22 +19,22 @@ get_header(); ?>
 				while( $teasers->have_posts() ) {
 					$teasers->the_post();
 					?>
-
 					<div class='content'>
 						<h2><?php the_title() ?></h2>
-
 						<img src="<?php the_field('teaser_image'); ?>" alt="Teaser image">
-
 						<p><?php the_field('teaser_summary'); ?></p>
-
 						<p><a href="<?php the_field('learn_more_link'); ?>">Learn more</a></p>
-
 					</div>
-					<?php
+					<?php if ( is_user_logged_in() ) { ?>
+						<a class="post-edit" href="<?php echo get_edit_post_link( $id, $context ); ?>">Edit this</a>
+					<?php }
 				}
 			}
 		?>
 		<!-- end teaser blocks -->
+
+		<!-- Start single quote block -->
+
 
 	</div>
 
