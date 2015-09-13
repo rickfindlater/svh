42a6f2b87e0e10bf1006d7e6c6069028688a7477
rickfindlater <rickfindlater@gmail.com>
2015-09-14 09:27:02 +1200

    footer menus

--- wp-content/themes/svh/library/widget-areas.php
+++ wp-content/themes/svh/library/widget-areas.php
@@ -23,10 +23,10 @@ function foundationpress_sidebar_widgets() {
 	  'id' => 'footer-widgets',
 	  'name' => __( 'Footer widgets', 'foundationpress' ),
 	  'description' => __( 'Drag widgets to this footer container', 'foundationpress' ),
-	  'before_widget' => '<article id="%1$s" class="large-4 columns widget %2$s">',
-	  'after_widget' => '</article>',
-	  'before_title' => '<h6>',
-	  'after_title' => '</h6>',
+	  'before_widget' => '<div id="%1$s" class="%2$s"><div class="row row-standard-padding">',
+	  'after_widget' => '</div></div>',
+	  'before_title' => '<h4>',
+	  'after_title' => '</h4>',
 	));
 }
 
