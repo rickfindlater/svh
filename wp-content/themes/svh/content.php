0da42dec5559bca66260e3d02146665751e86559
rickfindlater <rickfindlater@gmail.com>
2015-09-19 17:18:19 +1200

    setting up blog

--- wp-content/themes/svh/content.php
+++ wp-content/themes/svh/content.php
@@ -17,7 +17,12 @@
 		<?php foundationpress_entry_meta(); ?>
 	</header>
 	<div class="entry-content">
-		<?php the_content( __( 'Continue reading...', 'foundationpress' ) ); ?>
+		<!-- The post featured image -->
+		<?php the_post_thumbnail( $size, $attr ); ?>
+		<!-- The post excerpt -->
+		<?php the_excerpt(); ?>
+		<!-- Read more button -->
+		<a href="<?php the_permalink(); ?>">Read More.</a>
 	</div>
 	<footer>
 		<?php $tag = get_the_tags(); if ( $tag ) { ?><p><?php the_tags(); ?></p><?php } ?>
--- wp-content/themes/svh/index.php
+++ wp-content/themes/svh/index.php
@@ -34,8 +34,6 @@ get_header(); ?>
 
 	<?php endif;?>
 
-
-
 	<?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
 		<nav id="post-nav">
 			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
