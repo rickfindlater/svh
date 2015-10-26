a31ac062d06a51c166294a0fc46d19a9e9b23304
rickfindlater <rick@wipster.io>
2015-10-26 23:47:56 +1300

    background hero unit video stuff

--- wp-content/themes/svh/templates/page-homepage.php
+++ wp-content/themes/svh/templates/page-homepage.php
@@ -18,20 +18,25 @@ get_header(); ?>
 				BV.show('https://player.vimeo.com/external/142859789.sd.mp4?s=74321aa6204b13c6b4c243bc45cea0db&profile_id=112',{ambient:true});
 			}
 
-			$( "#video-play-button" ).click(function() {
+			//when modal is open we pause the background video and add the html of the full video with autoplay
+			$('#videoModal').on('open.fndtn.reveal', function(e) {
 				BV.getPlayer().pause();
+				$("#videoModal .flex-video").html('<iframe src="https://player.vimeo.com/video/142859791?autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>')
 			});
 
-			//BV.getPlayer().play();
+			//when modal is open we re start the background video and remove the html of the full video with autoplay
+			$('#videoModal').on('closed.fndtn.reveal', function(e) {
+				BV.getPlayer().play();
+				$("#videoModal .flex-video").html('');
+			});
 		});
 
 	</script>
 	<button id="video-play-button" data-reveal-id="videoModal" class="radius button">PLAY VIDEO</button>
 
-	<div id="videoModal" class="reveal-modal large" data-reveal="" aria-hidden="true" style="opacity: 1; visibility: hidden; display: none;">
-		<h2>This modal has video</h2>
+	<div id="videoModal" class="reveal-modal large" data-reveal="svhVideo" aria-hidden="true" style="opacity: 1; visibility: hidden; display: none;">
 		<div class="flex-video" style="display: none;">
-			<iframe width="300" height="300" src="https://www.youtube.com/embed/aiBt44rrslw" frameborder="0" allowfullscreen="" data-src="https://www.youtube.com/embed/aiBt44rrslw"></iframe>
+
 		</div>
 		<a class="close-reveal-modal">×</a>
 	</div>
