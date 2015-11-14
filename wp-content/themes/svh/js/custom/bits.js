81e5fde20b0e3cc162b5dda00bb2e8120f5dcb2e
rickfindlater <rick@wipster.io>
2015-11-14 17:23:29 +1300

    various fixes

--- wp-content/themes/svh/js/custom/bits.js
+++ wp-content/themes/svh/js/custom/bits.js
@@ -1,12 +1,8 @@
 // JS Bits and pieces
 
-(function($) {
-
-	//scroll to contact form on homepage
-	$("#scroll-contact-form").click(function() {
-		$('html, body').animate({
-			scrollTop: $("#contact-form-anchor").offset().top
-		}, 800);
-	});
-
-})(jQuery);
+//scroll to contact form on homepage
+$("#scroll-contact-form").click(function() {
+	$('html, body').animate({
+		scrollTop: $("#contact-form-anchor").offset().top
+	}, 800);
+});
