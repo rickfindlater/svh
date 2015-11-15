9e239eeffa48276574f3fdae531513a2b0e84d49
rickfindlater <rick@wipster.io>
2015-11-15 15:16:53 +1300

    making initial load a bit better

--- wp-content/themes/svh/js/custom/bits.js
+++ wp-content/themes/svh/js/custom/bits.js
@@ -1,8 +1,18 @@
 // JS Bits and pieces
 
-//scroll to contact form on homepage
-$("#scroll-contact-form").click(function() {
-	$('html, body').animate({
-		scrollTop: $("#contact-form-anchor").offset().top
-	}, 800);
+$(document).ready(function(){
+	//scroll to contact form on homepage
+	$("#scroll-contact-form").click(function() {
+		$('html, body').animate({
+			scrollTop: $("#contact-form-anchor").offset().top
+		}, 800);
+	});
+
+
+
+	function fadeHero(){
+		$(".hp-hero-inner").fadeIn();
+	}
+	setTimeout(fadeHero, 1000);
+
 });
