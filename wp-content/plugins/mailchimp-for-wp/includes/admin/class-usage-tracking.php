2a3d1cea106d88952504f32d184bd27d3ba3e5fc
rickfindlater <rick@wipster.io>
2015-11-25 11:51:49 +1300

    mailchimp modile updates

--- wp-content/plugins/mailchimp-for-wp/includes/admin/class-usage-tracking.php
+++ wp-content/plugins/mailchimp-for-wp/includes/admin/class-usage-tracking.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Class MC4WP_Usage_Tracking
+ *
+ * @access private
+ * @since 2.3
+ * @ignore
+ */
 class MC4WP_Usage_Tracking {
 
 	/**
@@ -34,25 +41,51 @@ class MC4WP_Usage_Tracking {
 	 */
 	public function add_hooks() {
 		add_action( 'mc4wp_usage_tracking', array( $this, 'track' ) );
+		add_filter( 'cron_schedules', array( $this, 'cron_schedules' ) );
+	}
+
+	/**
+	 * Registers a new schedule with WP Cron
+	 *
+	 * @param array $schedules
+	 *
+	 * @return array
+	 */
+	public function cron_schedules( $schedules ) {
+		$schedules['monthly'] = array(
+			'interval' => 30 * DAY_IN_SECONDS,
+			'display' => __( 'Once a month' )
+		);
+		return $schedules;
+	}
+
+	/**
+	 * Enable usage tracking
+	 */
+	public function enable() {
+		return wp_schedule_event( time(), 'monthly', 'mc4wp_usage_tracking' );
+	}
+
+	/**
+	 * Disable usage tracking
+	 */
+	public function disable() {
+		wp_clear_scheduled_hook( 'mc4wp_usage_tracking' );
 	}
 
 	/**
 	 * Toggle tracking (clears & sets the scheduled tracking event)
 	 *
-	 * @param bool $enabled
+	 * @param bool $enable
 	 */
-	public function toggle( $enabled ) {
-
-		if( ! $enabled ) {
-			wp_clear_scheduled_hook( 'mc4wp_usage_tracking' );
-			return;
-		}
-
-		wp_schedule_event( time(), 'daily', 'mc4wp_usage_tracking' );
+	public function toggle( $enable ) {
+		$enable ? $this->enable() : $this->disable();
 	}
 
 	/**
 	 * Sends the tracking request. Non-blocking.
+	 *
+	 * @return bool
 	 */
 	public function track() {
 		$data = $this->get_tracking_data();
@@ -67,6 +100,8 @@ class MC4WP_Usage_Tracking {
 				'blocking' => false,
 			)
 		);
+
+		return true;
 	}
 
 	/**
@@ -77,16 +112,35 @@ class MC4WP_Usage_Tracking {
 		$data = array(
 			// use md5 hash of home_url, we don't need/want to know the actual site url
 			'site' => md5( home_url() ),
-			'options' => $this->get_tracked_options(),
 			'number_of_mailchimp_lists' => $this->get_mailchimp_lists_count(),
-			'mc4wp_version' => MC4WP_LITE_VERSION,
+			'mc4wp_version' => $this->get_mc4wp_version(),
+			'mc4wp_premium_version' => $this->get_mc4wp_premium_version(),
 			'plugins' => (array) get_option( 'active_plugins', array() ),
 			'php_version' => PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION,
 			'curl_version' => $this->get_curl_version(),
-			'wp_version' => $GLOBALS['wp_version']
+			'wp_version' => $GLOBALS['wp_version'],
+			'wp_language' => get_locale(),
+			'server_software' => $this->get_server_software(),
+			'using_https' => $this->is_site_using_https()
 		);
 
 		return $data;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function get_mc4wp_premium_version() {
+		return defined( 'MC4WP_PREMIUM_VERSION' ) ? MC4WP_PREMIUM_VERSION : 0;
+	}
+
+	/**
+	 * Returns the MailChimp for WordPress version
+	 *
+	 * @return string
+	 */
+	protected function get_mc4wp_version() {
+		return MC4WP_VERSION;
 	}
 
 	/**
@@ -94,33 +148,10 @@ class MC4WP_Usage_Tracking {
 	 */
 	protected function get_mailchimp_lists_count() {
 		$mailchimp = new MC4WP_MailChimp();
-		$lists = $mailchimp->get_lists( false, true );
+		$lists = $mailchimp->get_lists( true );
 		return count( $lists );
 	}
 
-	/**
-	 * @return array
-	 */
-	public function get_tracked_options( ) {
-
-		$checkbox_options = mc4wp_get_options( 'checkbox' );
-		$form_options = mc4wp_get_options( 'form' );
-
-		// make sure these keys are always stripped
-		$ignored_options = array( 'api_key', 'license_key', 'lists' );
-
-		// filter options
-		$checkbox_options = array_diff_key( $checkbox_options, array_flip( $ignored_options ) );
-		$form_options = array_diff_key( $form_options, array_flip( $ignored_options ) );
-
-		// merge options
-		$options = array(
-			'checkbox' => $checkbox_options,
-			'form' => $form_options
-		);
-
-		return $options;
-	}
 
 	/**
 	 * @return string
@@ -133,5 +164,25 @@ class MC4WP_Usage_Tracking {
 
 		$curl_version_info = curl_version();
 		return $curl_version_info['version'];
+	}
+
+	/**
+	 * @return bool
+	 */
+	protected function is_site_using_https() {
+		$site_url = site_url();
+		return strpos( $site_url, 'https://' ) === 0;
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function get_server_software() {
+
+		if( ! isset( $_SERVER['SERVER_SOFTWARE'] ) ) {
+			return '';
+		}
+
+		return $_SERVER['SERVER_SOFTWARE'];
 	}
 }
--- wp-content/plugins/mailchimp-for-wp/vendor/autoload_52.php
+++ wp-content/plugins/mailchimp-for-wp/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit2f909f1fbc22fbc2d15e30f4553f3491::getLoader();
+return ComposerAutoloaderInit54e80846664a66adbb00c7cd0aeffbf5::getLoader();
