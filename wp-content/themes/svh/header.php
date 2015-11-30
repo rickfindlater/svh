f158b096825254b66592fe7deade48c2cdd70ca1
rickfindlater <rick@wipster.io>
2015-12-01 12:54:30 +1300

    adding google analytics tag

--- wp-content/themes/svh/header.php
+++ wp-content/themes/svh/header.php
@@ -22,6 +22,16 @@
 <!--		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="--><?php //echo get_stylesheet_directory_uri(); ?><!--/assets/img/icons/apple-touch-icon-72x72-precomposed.png">-->
 <!--		<link rel="apple-touch-icon-precomposed" href="--><?php //echo get_stylesheet_directory_uri(); ?><!--/assets/img/icons/apple-touch-icon-precomposed.png">-->
 
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+						(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+					m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-70839750-1', 'auto');
+			ga('send', 'pageview');
+
+		</script>
 		<meta name="google-site-verification" content="oUdCj2-6snBJgu9yqgSRX8hIRT4_Zo6_BEENIxevm8Q" />
 		<!--WRFL device detection-->
 		<script type="text/javascript" src="//wurfl.io/wurfl.js"></script>
